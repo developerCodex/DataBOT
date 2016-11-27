292e011fd7429718d8a1106e984b5a25d9bafedd
developerCodex <trapspanky@gmail.com>
2016-11-27 08:50:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
 // Discord.js Latest Version
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 const config = require("./config")
 const prefix = config.prefix
 
-client.on('ready', () => {
+bot.on('ready', () => {
   console.log('I am ready!');
-  client.user.setGame(config.setgame, "https://www.twitch.tv/discordapp")
+  bot.user.setGame(config.setgame, "https://www.twitch.tv/discordapp")
 });
 
-client.on('message', message => {
-if(message.content.startsWith(prefix + 'ping')) {
+bot.on('message', message => {
+if(message.content.startsWith(prefix + "ping")) {
     message.channel.sendMessage("Pong!");
   }
+  if(message.content.startsWith(prefix + "foo")) {
+    message.channel.sendMessage("bar!")
 });
 
-client.login(config.token);
+bot.login(config.token);
